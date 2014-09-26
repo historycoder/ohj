9c37dfc880d1c44656009e502d63252dc7c142c3
Phil Sager <psager@ohiohistory.org>
2014-09-25 21:59:59 -0400

    adding browse, reorganizing

--- index.php
+++ index.php
@@ -1,102 +1,50 @@
-<!doctype html public 
-  "-//w3c//dtd html 4.01 transitional//en"
-  "http://www.w3.org/tr/1999/rec-html401-19991224/loose.dtd">
-<html> 
+<!DOCTYPE html>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>  
-
-<title>Ohio History Journal</title>
-
-
+<title>OHJ Archive</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link media="ALL" rel="stylesheet" type="text/css" href="style.css">
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/json2/20121008/json2.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 
 <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.2.12/jquery.jgrowl.min.css" />
-<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.2.12/jquery.jgrowl.min.js"></script>
-
 <script type="text/javascript" src="formly/formly.js"></script>
 <link rel="stylesheet" href="formly/formly.css" type="text/css" />
 
-<script type="text/javascript">
-	
-	$(document).ready(function() {		
-		$('#searchform').formly(); 
-	});
-	
-	function validateForm() {
-		/*	
-		var yearExists = /^\d\d\d\d$/.test(document.forms["newssearch"]["year"].value);
-		var titleExists = /[A-Na-n]/.test(document.forms["newssearch"]["title"].value);
-		var searchtermExists = /[A-Na-n]/.test(document.forms["searchform"]["searchterm"].value);
-		
-		if (!yearExists && !titleExists && !authorExists && !subjectExists && !searchTerm) {
-			//new Messi('Need something to search', {title: 'Search Error', titleClass: 'info', buttons: [{id: 0, label: 'Close', val: 'X'}]});
-			$.jGrowl("Need something to search", { theme: 'validation', header: 'Search Error', live: 10000 });
-			return false;
-		}
-		if (!searchtermExists) {
-			$('#newsform').formly(); 
-		}
-		
-		*/
-		return true;
-	}
-</script>
-
-<style type="text/css">
-
-body {
-	font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-	font-size: 12px;
-}
-.heading {
-	text-align:center;
-	font-size:120%;padding:20px;
-}
-.backto {
-	text-align:center;
-	padding:10px;
-}
-a.navlinks {
-	text-decoration: none;
-}
-a.navlinks:hover {
-	color: red;
-	text-decoration: underline;
-}
-div.jGrowl div.validation {
-	background-color: #808080;
-	width: 200px;
-	min-height: 0px;
-	border: 1px solid #000;
-}
-
-</style>
-</head> 
+</head>
 <body>
-		
-	<div class="backto"><a class="navlinks" href="http://www.ohiohistory.org/collections--archives/archives-library">Library/Archives Home</a></div>
-	<div class="heading"><b><i>Ohio History Journal</i> Search</b></div>
+
+	<p>&nbsp;</p>
+	<div id="description" class="container">
+		<div style="width:80%;margin: 0 auto;">
+	<div id="heading" class="container"><h1>Ohio History Journal</h1></div>
+
 	
-	<div style="width:100%">
-	
-	<div style="width:38%;margin: 0 auto;">
-	
-	<form id="searchform" name="searchform" action="results.php" method="POST" onsubmit="return validateForm()"  style="width: 500px;margin: 0 auto">
-	
-		<br/>Full Text Search: <input type="text" name="searchterm" size="25" maxsize="50">
-		<!--<br/>Title:  <input type="text" name="title" size="25" maxsize="50">
-		<br/>Author:  <input type="text" name="author" size="25" maxsize="50">
-		<br/>Subject:  <input type="text" name="subject" size="25" maxsize="50">
-		<br/>Year: <input type="text" name="year" size="4" maxsize="4"> -->
-		<br/><input type="submit" value="Search" />
-		
-	</form>
-	<br/>
-	<p><b>The <i>Ohio History Journal</i> Search provides a means to search the full text of the Ohio History Journal from 1887 through 2004</b>.</p>
-	
+		<p><i><font face="Arial, Helvetica, sans-serif">Ohio History</font></i><font face="Arial, Helvetica, sans-serif"> 
+  was established in June 1887 as the <i>Ohio  Archaeological and Historical Quarterly</i>. For more than one hundred years, the journal has published peer-reviewed articles and book reviews on topics spanning the political, military, social, economic, and cultural history of our region.</font> <a name="KSU"></a></p>
+  <div id="maincontent" class="content">
+	<h2><em><b>JOURNAL TRANSFER TO KSU PRESS</b></em></h2>
+
+		<p><font face="Arial, Helvetica, sans-serif">Published by the Kent State University Press since 2007, <i>Ohio History</i> was the scholarly journal of the Ohio Historical Society for more than 100 years. The journal is an essential tool for researchers of Ohio and the Middle West. We are pleased to make available to you online a comprehensive archive of the journal, including text, footnotes, and images published from 1887 to 2000. Beginning in spring 2010, the 2007 volume year will be added to the OHS online archive, with subsequent volumes to be added annually.</p>
+
+		<p><font face="Arial, Helvetica, sans-serif"> KSU Press will produce one print issue of the journal per year, beginning with the Spring 2007/Volume 114 issue. Please check the <a href="http://upress.kent.edu/journals/index.htm">KSU Press website</a> for updates. Authors who wish to inquire about submitting articles may contact the editor, L. Diane Barnes, at <a href="mailto:dbarnes@ysu.edu">dbarnes@ysu.edu</a>.</font><a name="archive"></a></p>
+	<h2><em><b>ABOUT THE ONLINE ARCHIVE</b></em></h2>
+
+  <p><font face="Arial, Helvetica, sans-serif">The Ohio Historical Society is pleased to continue to provide a free and fully searchable online archive of <i>Ohio History</i>'s issues spanning from 1887 through 2004. This archive is available thanks to the generous sponsorship of the Ohio Public Library Information Network (<a href="http://www.oplin.lib.oh.us/">OPLIN</a>). For permission to reproduce material from this archive, please contact the permissions editor at <a href="mailto:ohiohistory@ohiohistory.org">ohiohistory@ohiohistory.org</a>. 
+  </font> 
+	</p>
+	<p><font face="Arial, Helvetica, sans-serif">Following are a few key points to keep in mind while navigating this archive: </font>
+	</p>
+	<blockquote> 
+		<p><font face="Arial, Helvetica, sans-serif">You can find information in a variety of ways. You may browse the alphabetical listings in the <a href="browse/indexbrowse.php">Index</a>, search for a particular topic in the <a href="browse/indexsearch.php">Index Search</a>, or try a <a href="search/index.php">Full-text Search</a> for more obscure subjects. You may also browse individual issues by <a href="browse/volumebrowse.php">Volume Number</a>. </font>
+		</p>
+		<p><font face="Arial, Helvetica, sans-serif">If you are a researcher citing a reference from <i>Ohio History</i>, please see <a href="issues.html">Appendix A: Issue Dates and Pagination</a>. </font>
+		</p>
+	</blockquote>
 	</div>
-	</div>
-	
+</div>
 </body>
 </html>
